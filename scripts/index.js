a92a0eb68e742add5400339726385ad42862431d
Evgeny Geyer <evgeny.geyer@ya.ru>
2021-01-13 14:33:21 +0300

    начальный этап организации кода по модулям

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 const buttonEdit = document.querySelector('.btn_type_edit'); // кнопка редактировать профиль 
 const buttonAdd = document.querySelector('.btn_type_add'); // кнопка добавить карточку
 const popupShowImage = document.querySelector('.popup_type_img');  // попап с увеличенной картинкой
-
+const popupProfileEdit = document.querySelector('.popup_type_edit'); // попап с формой редактирования профиля 
+const popupAddCard = document.querySelector('.popup_type_add'); // попап с формой добавления карточки.
 const buttonCloseEditPopup = popupProfileEdit.querySelector('.btn_type_close'); // кнопка закрыть попап редактирования 
 const buttonCloseAddPopup = popupAddCard.querySelector('.btn_type_close');  // кнопка закрыть попап добавления
 const buttonCloseImgPopup = popupShowImage.querySelector('.btn_type_close'); // кнопка закрыть попап большого изображения
@@ -16,9 +17,118 @@ const titleInput = popupAddCard.querySelector('.popup__input_text_title'); // п
 const linkInput = popupAddCard.querySelector('.popup__input_text_link'); // поле ввода ссылки на изображение
 const profileName = document.querySelector('.profile__name'); // имя профиля показанное на странице
 const profileJob = document.querySelector('.profile__about-me'); // вид деятельности показанный на странице
+const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+};
+
+
+const formEditElement = popupProfileEdit.querySelector('.popup__form'); // форма редактирования профиля
+const formAddElement = popupAddCard.querySelector('.popup__form'); // форма добавления элемента
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+class FormValidator {
+  constructor(data, form) {
+    this._form = form;
+    this._input = data.inputSelector;
+    this._submitButton = data.submitButtonSelector;
+    this._inactiveButton = data.inactiveButtonClass;
+    this._inputError = data.inputErrorClass;
+  }
+
+  _showError(input) {
+    this._error = this._form.querySelector(`#${input.id}-error`);
+    this._error.textContent = input.validationMessage;
+    input.classList.add(this._inputError);
+  }
+
+  _hideError(input) {
+    this._error = this._form.querySelector(`#${input.id}-error`);
+    this._error.textContent = "";
+    input.classList.remove(this._inputError);
+  }
+
+  _checkInvalidInput() {
+    return this._inputList.some((input) => {
+      return !input.validity.valid;
+    })
+  }
+
+  _checkInputValidity(input) {
+    if (input.validity.valid) {
+      this._hideError(input);
+    } else {
+      this._showError(input);
+    }
+  }
+
+  _setButtonState() {
+    if (!this._checkInvalidInput()) {
+      this._button.classList.remove(this._inactiveButton);
+      this._button.disabled = false;
+    } else {
+      this._button.classList.add(this._inactiveButton);
+      this._button.disabled = true;
+    }
+  }
+
+  _setEventListener() {
+    this._inputList = [...this._form.querySelectorAll(this._input)];
+    this._button = this._form.querySelector(this._submitButton);
+
+    this._inputList.forEach((input) => {
+      input.addEventListener("input", () => {
+        this._checkInputValidity(input);
+        this._setButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._setEventListener();
+    this._setButtonState();
+  }
+}
+
+
+
+const validatorEdit = new FormValidator(validationConfig, formEditElement);
+const validatorAdd = new FormValidator(validationConfig, formAddElement);
+
 
 class Card {
-  constructor(data) {
+  constructor(data, template, popupImage, descriptionPopup,) {
     this._title = data.name;
     this._image = data.link;
   }
